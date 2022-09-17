3c5a3b0fb68fa2d9756069ca385503e53ae04c8b
Massimo Iacolare <info@massimoiacolare.it>
2022-09-17 19:38:06 +0200

    Add FetchACat skeleton

---
+++ src/app/FetchACat.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const FetchACat: React.FC = () => {
+    return (
+        <div>
+            <h1>Fetch-a-Cat</h1>
+        </div>
+    )
+}
--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,13 @@
 import React from "react"
+import { FetchACat } from "./FetchACat"
 
 const App: React.FC = () => {
-    return <h1>Welcome to Socrates 2022</h1>
+    return (
+        <>
+            <h1>Welcome to Socrates 2022</h1>
+            <FetchACat />
+        </>
+    )
 }
 
 export default App
