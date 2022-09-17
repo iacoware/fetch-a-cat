65c7200d670b3851c0a44f73aeabfe7ba7000164
Massimo Iacolare <info@massimoiacolare.it>
2022-09-17 20:53:39 +0200

    Add isLoading

--- src/app/FetchACat.tsx
+++ src/app/FetchACat.tsx
@@ -4,20 +4,30 @@ import Masonry from "react-responsive-masonry"
 
 export const FetchACat: React.FC = () => {
     const [cats, setCats] = useState<Cat[]>([])
+    const [isLoading, setIsLoading] = useState<boolean>(false)
 
     const onFetch = () => {
-        fetchCats().then(setCats)
+        setIsLoading(true)
+        fetchCats()
+            .then(setCats)
+            .then((c) => {
+                setIsLoading(false)
+                return c
+            })
     }
 
     return (
         <div className="app-container">
             <h1>Fetch-a-Cat</h1>
             <button onClick={onFetch}>Fetch</button>
-            <Masonry columnsCount={3}>
-                {cats?.map((c) => (
-                    <img key={c.id} src={c.url} />
-                ))}
-            </Masonry>
+            {isLoading && <div>Cats are coming, please be patient</div>}
+            <div className={isLoading ? "is-loading" : ""}>
+                <Masonry columnsCount={3}>
+                    {cats?.map((c) => (
+                        <img key={c.id} src={c.url} />
+                    ))}
+                </Masonry>
+            </div>
         </div>
     )
 }
