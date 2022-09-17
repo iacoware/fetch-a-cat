c1b3b44030b9e78d89d1d17d0e4fe4198c489eba
Massimo Iacolare <info@massimoiacolare.it>
2022-09-17 21:00:08 +0200

    Add error handling

--- src/app/FetchACat.tsx
+++ src/app/FetchACat.tsx
@@ -5,22 +5,31 @@ import Masonry from "react-responsive-masonry"
 export const FetchACat: React.FC = () => {
     const [cats, setCats] = useState<Cat[]>([])
     const [isLoading, setIsLoading] = useState<boolean>(false)
+    const [error, setError] = useState<unknown>(null)
 
     const onFetch = () => {
         setIsLoading(true)
+        setError(null)
         fetchCats()
             .then(setCats)
             .then((c) => {
                 setIsLoading(false)
                 return c
             })
+            .catch((error) => {
+                setIsLoading(false)
+                setError(error)
+            })
     }
+
+    const isError = !!error
 
     return (
         <div className="app-container">
             <h1>Fetch-a-Cat</h1>
             <button onClick={onFetch}>Fetch</button>
             {isLoading && <div>Cats are coming, please be patient</div>}
+            {isError && <div>We have an error, sorry</div>}
             <div className={isLoading ? "is-loading" : ""}>
                 <Masonry columnsCount={3}>
                     {cats?.map((c) => (
