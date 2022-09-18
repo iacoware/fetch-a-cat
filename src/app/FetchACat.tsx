2e9e96f21a2ec8199342d63a2dff21db7437a600
Massimo Iacolare <info@massimoiacolare.it>
2022-09-18 16:02:01 +0200

    Add fetch button

--- src/app/FetchACat.tsx
+++ src/app/FetchACat.tsx
@@ -1,9 +1,23 @@
-import React from "react"
+import React, { useState } from "react"
+import { Cat, fetchCats } from "../common/api"
+import Masonry from "react-responsive-masonry"
 
 export const FetchACat: React.FC = () => {
+    const [cats, setCats] = useState<Cat[]>([])
+
+    const onFetch = () => {
+        fetchCats().then(setCats)
+    }
+
     return (
-        <div>
+        <div className="app-container">
             <h1>Fetch-a-Cat</h1>
+            <button onClick={onFetch}>Fetch</button>
+            <Masonry columnsCount={3}>
+                {cats?.map((c) => (
+                    <img key={c.id} src={c.url} />
+                ))}
+            </Masonry>
         </div>
     )
 }
